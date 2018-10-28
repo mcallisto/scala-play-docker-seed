56fc2ac532d34e77839915556aec4cd6b15c03ed
gumaz <giuliomazzeo@gmail.com>
2018-10-28 18:11:13 +0100

    change task interval and delay

--- app/tasks/RecurrentTask.scala
+++ app/tasks/RecurrentTask.scala
@@ -2,16 +2,14 @@ package tasks
 
 import akka.actor.ActorSystem
 import com.google.inject.Inject
-import scala.concurrent.duration._
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 class RecurrentTask@Inject() (actorSystem: ActorSystem)(implicit executionContext: ExecutionContext) {
+  val interval: FiniteDuration = 3.seconds
 
-  val interval = 5.seconds
-
-  actorSystem.scheduler.schedule(initialDelay = 10.seconds, interval = interval) {
-    // the block of code that will be executed
-    print(s"Hello! I'm printing this every $interval seconds...")
+  actorSystem.scheduler.schedule(initialDelay = 2.seconds, interval = interval) {
+    println(s"Hello! I'm printing this every $interval seconds...")
   }
 }
